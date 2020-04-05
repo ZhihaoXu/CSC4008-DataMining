0c3960e1ef8cf7da6e51a6a4f5f0b9f4b3fd1283
ZhihaoXu <117010330@link.cuhk.edu.cn>
2020-04-05 21:42:17 +0800

    add apriori fp-growth

---
+++ Apriori.py
@@ -0,0 +1,79 @@
+'''
+Created on April 3, 2020
+Apriori Algorithm
+@author: ZhihaoXu
+'''
+import math
+import pandas as pd
+import numpy as np
+from tqdm import tqdm 
+
+
+def loadDataSet():
+    xls_file=pd.read_excel('AS5supermarket.xlsx')
+    data = np.array(xls_file)
+    np.delete(data, [216], axis=1)
+    product = []
+    for i in range(data.shape[0]):
+        product.append(np.where(data[i,]==1)[0])
+    return product
+
+def createC1(dataSet):
+    C1 = []
+    for transaction in dataSet:
+        for item in transaction:
+            if not [item] in C1:
+                C1.append([item])
+                
+    C1.sort()
+    return list(map(frozenset, C1))#use frozen set so we can use it as a key in a dict    
+
+def scanD(D, Ck, minSupport):
+    ssCnt = {}
+    for tid in D:
+        for can in Ck:
+            if can.issubset(tid):
+                if can not in ssCnt: ssCnt[can]=1
+                else: ssCnt[can] += 1
+    numItems = float(len(D))
+    retList = []
+    supportData = {}
+    for key in ssCnt:
+        support = ssCnt[key]/numItems
+        if support >= minSupport:
+            retList.insert(0,key)
+        supportData[key] = support
+    return retList, supportData
+
+def aprioriGenerate(Lk, k): #creates Ck
+    retList = []
+    lenLk = len(Lk)
+    for i in range(lenLk):
+        for j in range(i+1, lenLk): 
+            L1 = list(Lk[i])[:k-2]; L2 = list(Lk[j])[:k-2]
+            L1.sort(); L2.sort()
+            if L1==L2: #if first k-2 elements are equal
+                retList.append(Lk[i] | Lk[j]) #set union
+    return retList
+
+def apriori(dataSet, minSupport = 0.5):
+    C1 = createC1(dataSet)
+    D = list(map(set, dataSet))
+    L1, supportData = scanD(D, C1, minSupport)
+    L = [L1]
+    k = 2
+    while (len(L[k-2]) > 0):
+        Ck = aprioriGenerate(L[k-2], k)
+        Lk, supK = scanD(D, Ck, minSupport)#scan DB to get Lk
+        supportData.update(supK)
+        L.append(Lk)
+        k += 1
+    return L, supportData 
+            
+if __name__ == "__main__":
+    dataSet = loadDataSet()
+    minSupport = 0.5
+    Lk, suppData = apriori(dataSet, minSupport)
+    
+
+    
